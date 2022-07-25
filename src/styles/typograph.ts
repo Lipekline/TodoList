7967bfd41eebbbd143f8cfc69d34c22bb5f44340
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2022-07-25 11:11:08 -0300

    :construction: Implementa arquivos de tipografia :construction:

---
+++ src/styles/typograph.ts
@@ -0,0 +1,12 @@
+import { scaleFont } from './mixins';
+
+export const FONT_SIZE_SMALL = scaleFont(12);
+export const FONT_SIZE_PARAGRAPH = scaleFont(14);
+export const FONT_SIZE_H0 = scaleFont(26);
+export const FONT_SIZE_H1 = scaleFont(22);
+export const FONT_SIZE_H2 = scaleFont(18);
+export const FONT_SIZE_H3 = scaleFont(16);
+
+export const LINE_HEIGHT_24 = scaleFont(24);
+export const LINE_HEIGHT_20 = scaleFont(20);
+export const LINE_HEIGHT_16 = scaleFont(16);
