2764e293e3a0057452ee85d8b1fb0ace6dc7e9de
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2022-07-31 23:38:54 -0300

    Ajusta espaçamento das importações

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 
 import TabRoutes from './tabs.routes';
 
 const AppRoutes: React.FC = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
         <TabRoutes />
       </NavigationContainer>
