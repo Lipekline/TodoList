36636d307f017a1515d779d27bcc3b08881552dd
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2022-07-31 23:08:09 -0300

    :construction: Implementa icones na barra de navegação :construction:

--- src/routes/tabs.routes.tsx
+++ src/routes/tabs.routes.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
+import { scaleSize } from '../styles/mixins';
+import { COLORS } from '../styles/colors';
+import IconHomeActive from '../assets/icons/icon-home-active.svg';
+import IconHomeInative from '../assets/icons/icon-home-inative.svg';
+import IconProfileActive from '../assets/icons/icon-profile-active.svg';
+import IconProfileInative from '../assets/icons/icon-profile-inative.svg';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,12 +15,28 @@ const TabRoutes: React.FC = () => {
   return (
     <Tab.Navigator
       screenOptions={{
+        tabBarStyle: {
+          paddingBottom: scaleSize(16),
+          paddingTop: scaleSize(16),
+          height: scaleSize(73),
+        },
+        tabBarLabelStyle: { marginTop: scaleSize(5), fontSize: scaleSize(12) },
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.gray,
+        headerStyle: {
+          backgroundColor: COLORS.primary,
+        },
         headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: COLORS.white,
+        },
       }}>
       <Tab.Screen
         name="HomeScreen"
         options={{
           title: 'Home',
+          tabBarIcon: ({ focused }) =>
+            focused ? <IconHomeActive /> : <IconHomeInative />,
         }}
         component={Home}
       />
@@ -22,6 +44,8 @@ const TabRoutes: React.FC = () => {
         name="ProfileScreen"
         options={{
           title: 'Perfil',
+          tabBarIcon: ({ focused }) =>
+            focused ? <IconProfileActive /> : <IconProfileInative />,
         }}
         component={Profile}
       />
