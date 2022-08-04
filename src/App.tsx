41a9ea3f315b36c86fc4d64ede9e14c58a09bebe
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2022-08-04 11:19:45 -0300

    Adiciona SafeAreaView e StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import AppRoutes from './routes';
 import SplashScreen from 'react-native-splash-screen';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { COLORS } from './styles/colors';
 
 export default function App() {
   useEffect(() => {
@@ -9,6 +11,8 @@ export default function App() {
 
   return (
     <>
+      <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
+      <SafeAreaView style={{ backgroundColor: COLORS.primary }} />
       <AppRoutes />
     </>
   );
